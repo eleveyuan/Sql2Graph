e5c8465b3f6aebe07b029e54e9df3b9ca458cf05
eleveyuan <eleveyuan@gmail.com>
2022-03-29 19:43:44 +0800

    some wrap thing

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
