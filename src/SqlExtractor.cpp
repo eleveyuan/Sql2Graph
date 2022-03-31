2bf584d050d859ef93ddb4481e68e31f2001b464
eleveyuan <eleveyuan@gmail.com>
2022-03-31 22:53:03 +0800

    add extractor

---
+++ src/SqlExtractor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 猿木 on 2022/3/31.
+//
+
+#include "SqlExtractor.h"
---
+++ src/SqlExtractor.h
@@ -0,0 +1,14 @@
+//
+// Created by 猿木 on 2022/3/31.
+//
+
+#ifndef SQL2GRAPH_SQLEXTRACTOR_H
+#define SQL2GRAPH_SQLEXTRACTOR_H
+
+
+class SqlExtractor {
+
+};
+
+
+#endif //SQL2GRAPH_SQLEXTRACTOR_H
