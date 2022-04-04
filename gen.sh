257e6a5a118dea757e9be9002231d581df4f50d5
eleveyuan <eleveyuan@gmail.com>
2022-04-04 22:16:40 +0800

    solution issue

--- gen.sh
+++ gen.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 LOCATION=antlr-4.9.3-complete.jar
-java -jar $LOCATION -Dlanguage=Cpp -listener -visitor -o generated/mysql/ resource/mysql/MySqlLexer.g4 resource/mysql/MySqlParser.g4
+# -Xexact-output-dir for issue(https://github.com/antlr/antlr4/issues/638 and https://github.com/antlr/antlr4/pull/2065)
+java -jar $LOCATION -Dlanguage=Cpp -listener -visitor -Xexact-output-dir -o generated/mysql/ resource/mysql/MySqlLexer.g4 resource/mysql/MySqlParser.g4
 
